71f9ac4c854bded8b28da0c48a1098a5450492ae
1F10250107 <s1f102501079@iniad.org>
2026-06-25 11:06:25 +0900

    修正

--- todo/tests.py
+++ todo/tests.py
@@ -6,6 +6,10 @@ from todo.models import Task
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_create_task1(self):
+        self.assertEqual(1 + 2, 3)
+
+class TaskModelTestCase(TestCase):
+    def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         task = Task(title="task1", due_at=due)
         task.save()
